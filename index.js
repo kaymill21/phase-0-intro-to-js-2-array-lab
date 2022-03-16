633cf49accf2c3f1ef5f5343d187ef4bab0d03ec
kayla <kay.ann2000@gmail.com>
2022-03-16 15:43:27 -0600

    commit tree 49c46c54b742cb792f203b4931d3f4c50786850f

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph")
+}
+function destructivelyPrependCat(name) {
+    cats.unshift('Bob')
+}
+function  destructivelyRemoveLastCat() {
+    cats.pop(-1)
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift(-4)
+}
+function  appendCat() {
+    console.log(moreCats = [...cats, 'Broom'])
+}
